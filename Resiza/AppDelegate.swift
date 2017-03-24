1791cc6efd40279d4baeac3cef2b72630979b378
Davide Bertola <dade@dadeb.it>
2017-03-24 09:17:36 +0100

    progress

--- Resiza/AppDelegate.swift
+++ Resiza/AppDelegate.swift
@@ -1,27 +1,71 @@
 //
 //  AppDelegate.swift
-//  Resiza
+//  Docka
 //
-//  Created by Davide Bertola on 10/02/2017.
-//  Copyright © 2017 Davide Bertola. All rights reserved.
+//  Created by Davide Bertola on 19/11/2016.
+//  Copyright © 2016 Davide Bertola. All rights reserved.
 //
 
 import Cocoa
+import CoreVideo
+import Silica
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        let app:NSApplication = NSApplication.shared()
+        app.setActivationPolicy(.prohibited)
+        let window:NSWindow = app.windows[0]
+        window.setIsVisible(false)
+        
+        AppDelegate.windowsRefit()
+        NSApp.terminate(nil)
     }
-
-
+    
+    class func windowsRefit() {
+        let app = NSWorkspace.shared().frontmostApplication
+        
+        if let siApp = SIApplication(runningApplication: app) {
+            let windows = siApp.visibleWindows() as! [SIWindow]
+            for window in windows {
+                if window.isNormalWindow() {
+                    let key = siApp.title() + window.title()
+                    var index = (UserDefaults.standard.value(forKey: key) as! Int?) ?? 0
+                    
+                    let screen = window.screen()!.frameWithoutDockOrMenu()
+                    let candidates = candidatesForScreen(screen: screen)
+                    let candidate = candidates[index % 3]
+                    window.setFrame(candidate)
+                    
+                    index += 1
+                    UserDefaults.standard.setValue(index, forKey: key)
+                    UserDefaults.standard.synchronize()
+                }
+            }
+        }
+    }
+    
+    class func candidatesForScreen(screen:CGRect) -> [CGRect]  {
+        let frameLarge = screen.insetBy(
+            dx: screen.width * 0.02,
+            dy: screen.height * 0.03
+        )
+        let frameMedium = screen.insetBy(
+            dx: screen.width * 0.1,
+            dy: screen.height * 0.1
+        )
+        let frameSmall = screen.insetBy(
+            dx: screen.width * 0.2,
+            dy: screen.height * 0.2
+        )
+        let candidates = [frameLarge, frameMedium, frameSmall]
+        return candidates
+    }
+    
 }
 
